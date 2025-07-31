d53618a1d9939c3540afa60a10823c0f71919706
yangpq <2825925353@qq.com>
2025-07-31 23:06:17 +0800

    feat: 初始化网站

---
+++ .vitepress/config.mjs
@@ -0,0 +1,69 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "daricyang's 知识库网站",
+  description: "知识库网站",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outlineTitle: '大纲', // 文档内容目录标题
+    outline: [2, 6], // 显示的标题层级范围
+    logo: '/assets/logo.png', // 首页左上角logo
+    sidebar: true, // 关闭侧边栏，同时要删除sidebar配置
+    aside: 'right', // 文档内容目录位置
+
+    // 顶部导航栏
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '工具',
+        items: [
+          { text: 'VS Code插件', link: '/markdown-examples' },
+          { text: 'Git指南', link: '/api-examples' },
+          { text: '常用工具网站链接', link: '/api-examples' }
+        ]
+      },
+      { text: '前端',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+    ],
+
+    // 侧边栏文档索引目录
+    sidebar: [
+      {
+        text: '工具',
+        items: [
+          { text: 'VS Code插件', link: '/markdown-examples' },
+          { text: 'Git指南', link: '/api-examples' },
+          { text: '常用工具网站链接', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '前端',
+        items: [
+          { text: '示例', link: '/markdown-examples' },
+          { text: 'api示例', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'ZNS SSD实验指南',
+        items: [
+          { text: '示例', link: '/markdown-examples' },
+          { text: 'api示例', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    // 社交媒体链接
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    // 底部
+    footer: {
+      copyright: 'Copyright © 2025-present Daric'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,38 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  # home页大标题 
+  name: "Daric's 知识库"
+
+  # home页副标题
+  # text: "笔记知识库"
+
+  # home页座右铭
+  tagline: 好记性不如烂笔头✏
+
+  # home页右侧图片
+  image:
+    src: /assets/background.png
+    alt: 学习永无止境
+  
+  # home页按钮
+  actions:
+    - theme: brand
+      text: 前端知识库
+      link: /markdown-examples
+    - theme: alt
+      text: ZNS SSD实验指南
+      link: /api-examples
+
+# home页下方小提示框
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
